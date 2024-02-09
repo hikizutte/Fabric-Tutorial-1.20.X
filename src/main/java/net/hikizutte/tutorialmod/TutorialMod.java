31b6effb4dd216dcdfece8c9b1f016f8063c3647
hikizutte <mousi11128@gmail.com>
2024-02-09 10:37:29 +0900

    ルビー、ルビーの原石を追加

--- src/main/java/net/hikizutte/tutorialmod/TutorialMod.java
+++ src/main/java/net/hikizutte/tutorialmod/TutorialMod.java
@@ -2,16 +2,17 @@ package net.hikizutte.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.hikizutte.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "turorialmod";
+	public static final String MOD_ID = "tutorialmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 		//LOGGER.info("Hello Fabric world!");
 	}
 }
