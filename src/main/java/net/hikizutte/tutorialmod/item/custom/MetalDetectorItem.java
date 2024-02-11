e32e68d7e8b02d703d9679b08716193fc4d596fd
hikizutte <mousi11128@gmail.com>
2024-02-11 09:36:06 +0900

    tagの追加

--- src/main/java/net/hikizutte/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/hikizutte/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,5 +1,6 @@
 package net.hikizutte.tutorialmod.item.custom;
 
+import net.hikizutte.tutorialmod.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -67,7 +68,7 @@ public class MetalDetectorItem  extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE);
+        return state.isIn(ModTags.Blocks.METAL_DETECTOR_DETECTIVE_BLOCKS);
     }
 
     @Override
