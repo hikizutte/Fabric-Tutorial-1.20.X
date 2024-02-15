a25c571bd2584c3ebabde7c919e02d14700af105
hikizutte <mousi11128@gmail.com>
2024-02-15 10:32:57 +0900

    村人（音楽家）、その他取引レシピの追加。

--- src/main/java/net/hikizutte/tutorialmod/TutorialMod.java
+++ src/main/java/net/hikizutte/tutorialmod/TutorialMod.java
@@ -7,7 +7,9 @@ import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.hikizutte.tutorialmod.block.ModBlocks;
 import net.hikizutte.tutorialmod.item.ModItems;
 import net.hikizutte.tutorialmod.item.ModItemsGroups;
+import net.hikizutte.tutorialmod.util.ModCustomTrades;
 import net.hikizutte.tutorialmod.util.ModLootTableModifiers;
+import net.hikizutte.tutorialmod.villager.ModVillagers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,6 +25,9 @@ public class TutorialMod implements ModInitializer {
 		ModBlocks.registerModBlocks();
 
 		ModLootTableModifiers.modifyLootTables();
+		ModCustomTrades.registerCustomTrades();
+
+		ModVillagers.registerVillagers();
 
 		FuelRegistry.INSTANCE.add(ModItems.COAL_BRIQUETTE,200);
 	}
