0457a176a0011c6ea9999cb500eba2fbafc64603
hikizutte <mousi11128@gmail.com>
2024-02-16 09:25:15 +0900

    1.20.1から1.20.2へupdate。

--- src/main/java/net/hikizutte/tutorialmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/hikizutte/tutorialmod/datagen/ModRecipeProvider.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.hikizutte.tutorialmod.block.ModBlocks;
 import net.hikizutte.tutorialmod.item.ModItems;
+import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
@@ -27,7 +28,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     }
 
     @Override
-    public void generate(Consumer<RecipeJsonProvider> exporter) {
+    public void generate(RecipeExporter exporter) {
         offerSmelting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
                 0.7f,200,"ruby");
         offerBlasting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
--- src/main/java/net/hikizutte/tutorialmod/util/ModLootTableModifiers.java
+++ src/main/java/net/hikizutte/tutorialmod/util/ModLootTableModifiers.java
@@ -49,6 +49,7 @@ public class ModLootTableModifiers {
         }));
 
         LootTableEvents.REPLACE.register((resourceManager, lootManager, id, original, source) -> {
+            /*
             if(SUSPICIOUS_SAND_ID.equals(id)) {
                 List<LootPoolEntry> entries = new ArrayList<>(Arrays.asList(original.pools[0].entries));
                 entries.add(ItemEntry.builder(ModItems.METAL_DETECTOR).build());
@@ -57,8 +58,9 @@ public class ModLootTableModifiers {
                 LootPool.Builder pool = LootPool.builder().with(entries);
                 return LootTable.builder().pool(pool).build();
             }
-
+            */
             return null;
+
         });
     }
 }
